193ca266bb31f8b91dc4fd77da06fb8e732c7140
Palmer Harrell <palmerharrell@me.com>
2016-03-22 13:28:21 -0500

    Tested POST to Firebase. Added a delete icon that doesn't do anything yet.

--- app/controllers/frontCtrl.js
+++ app/controllers/frontCtrl.js
@@ -1,8 +1,10 @@
 app.controller("frontCtrl", [
   "$scope",
+  "$http",
+  "firebaseURL",
   "getFactory",
   
-  function($scope, getFactory) {
+  function($scope, $http, firebaseURL, getFactory) {
     
     console.log("frontCtrl.js is running.");
 
@@ -35,6 +37,29 @@ app.controller("frontCtrl", [
 
     $scope.addNewItem = function() {
       console.log("newItem: ", $scope.newItem);
+
+      $http.post(firebaseURL + '/items.json',
+
+        JSON.stringify({
+          id: $scope.newItem.id,
+          fbuid: $scope.newItem.fbuid,
+          name: $scope.newItem.name,
+          type: $scope.newItem.type,
+          finished: $scope.newItem.finished,
+          recommended: $scope.newItem.recommended,
+          notes: $scope.newItem.notes,
+          rating: $scope.newItem.rating,
+          date: $scope.newItem.date
+
+        }))
+      .then(
+        function() {  // Handle RESOLVE
+          $scope.loadFromFirebase(); // Reload Firebase db
+        },
+        function(response) {  // Handle REJECT
+          console.log("POST Rejected:", response);
+        }
+      );
     };
 
     $scope.loadFromFirebase(); // Get list on page load
