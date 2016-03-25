e844341f8e6bf83d7d42dc49d14190f46d3ec97e
Palmer Harrell <palmerharrell@me.com>
2016-03-25 10:39:59 -0500

    Cleaned up indentation and removed commented out code

--- app/directives/customDirectives.js
+++ app/directives/customDirectives.js
@@ -6,7 +6,7 @@ app.directive('ngEnter', function () {
         scope.$apply(function (){
           scope.$eval(attrs.ngEnter);
         });
-		event.preventDefault();
+		    event.preventDefault();
       }
     });
   };
@@ -28,43 +28,42 @@ app.directive('ngEsc', function () {
 
 // focus on show directive
 app.directive('focusOnShow', function($timeout) {
-    return {
-        restrict: 'A',
-        link: function($scope, $element, $attr) {
-            if ($attr.ngShow){
-                $scope.$watch($attr.ngShow, function(newValue){
-                    if(newValue){
-                        $timeout(function(){
-                            $element.focus();
-                        }, 0);
-                    }
-                });      
-            }
-            if ($attr.ngHide){
-                $scope.$watch($attr.ngHide, function(newValue){
-                    if(!newValue){
-                        $timeout(function(){
-                            $element.focus();
-                        }, 0);
-                    }
-                });      
-            }
-
-        }
-    };
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+      if ($attr.ngShow){
+        $scope.$watch($attr.ngShow, function(newValue){
+          if(newValue){
+            $timeout(function(){
+              $element.focus();
+            }, 0);
+          }
+        });      
+      }
+      if ($attr.ngHide){
+        $scope.$watch($attr.ngHide, function(newValue){
+          if(!newValue){
+            $timeout(function(){
+              $element.focus();
+            }, 0);
+          }
+        });      
+      }
+    }
+  };
 });
 
 // Select input text when input gains focus
 app.directive('selectOnFocus', ['$window', function ($window) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            element.on('focus', function () {
-                if (!$window.getSelection().toString()) {
-                    // Required for mobile Safari
-                    this.setSelectionRange(0, this.value.length);
-                }
-            });
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element.on('focus', function () {
+        if (!$window.getSelection().toString()) {
+          // Required for mobile Safari
+          this.setSelectionRange(0, this.value.length);
         }
-    };
+      });
+    }
+  };
 }]);
